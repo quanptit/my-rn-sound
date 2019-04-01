9019d81e7155e0c36c69e07ba0fa9695e0800928
Quân trịnh văn <quanptit410@gmail.com>
2019-04-01 15:22:52 +0700

    fist

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+export {AudioPlayControl} from "./components/audioPlayControl/AudioPlayControl";
+export {default as SeekBarAudioPlayer} from "./components/audioPlayControl/SeekBarAudioPlayer";
+export {default as BtnPausePlay} from "./components/audioPlayControl/BtnPausePlay";
 export {default as RNSound} from "./RNSound";
 export {AppPlaySoundUtils} from "./AppPlaySoundUtils";
 export {ImvPlayAudio} from "./ImvPlayAudio";
